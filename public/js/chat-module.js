4cedf0f711be4e81eeaecc2bff605d1f7101b7a9
Fabian Johansson <fabulo1998@gmail.com>
2019-12-03 22:19:51 +0100

    added event listeners and stylings to message module

--- public/js/chat-module.js
+++ public/js/chat-module.js
@@ -1,9 +1,10 @@
 export class ChatModule {
     constructor(message, alias, avatar, date) {
+        //Creates HTML elements
         this.html = {
             container: document.createElement('msg-container'),
             message: document.createElement('p'),
-            user: document.createElement('h1'),
+            alias: document.createElement('h1'),
             date: document.createElement('span'),
             avatar: document.createElement('img'),
             editBtn: document.createElement('edit-btn'),
@@ -17,8 +18,22 @@ export class ChatModule {
             avatar: avatar,
             date: date
         }
+
+        this.setupEventListeners();
     }
 
+    //Registrers unique event listeners for each button in each message container
+    setupEventListeners() {
+        this.html.editBtn.addEventListener('click', () => {
+            console.log('clicked edit')
+        });
+
+        this.html.deleteBtn.addEventListener('click', () => {
+            console.log('clicked delete')
+        });
+    }
+
+    //Appends message to target node
     render(targetNode) {
         this.html.avatar.setAttribute('src', this.content.avatar);
         this.html.message.innerText = this.content.message;
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,26 @@
-import 
+import { ChatModule } from "./chat-module.js";
+
+function log() {
+    console.log('clicked');
+}
+
+let chatModule = new ChatModule(
+    'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.', 
+    'DragonSlayer123',
+    'localhost:3000',
+    '11:26',
+    log,
+    log
+);
+
+let chatModule2 = new ChatModule(
+    'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.', 
+    'DragonSlayer123',
+    'localhost:3000',
+    '11:26',
+    log,
+    log
+);
+
+chatModule.render(document.querySelector('body'));
+chatModule2.render(document.querySelector('body'));
