96fbb8975ed9549a06a40fa05a5ccd4bb63d7aa6
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-12-04 20:31:23 +0100

    Changed app.js filename to server.js and server.js to app.js

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var app = express();
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/chat', (req, res) => {
+    res.render('chat.ejs');
 });
 
-app.listen(3000);
+io.on('connection', (socket) => {
+    socket.on('newUser', (user) => {
+        socket.username = user;
+        console.log(user + ' connected');
 
-module.exports = app;
+        //New user is online
+        socket.broadcast.emit('newUser', `${user}: Is now online!`);
+
+        //You are online
+        socket.on('userOnline', (user) => {
+            socket.emit('userOnline', `You ${user} are online`);
+        });
+
+        // is Typing
+        socket.on('typing', (isTyping) => {
+            socket.broadcast.emit('updateTyping', user, isTyping);
+        });
+    });
+    socket.on('chat message', function (msg) { //Lyssnar på eventet 'chat message'
+        socket.broadcast.emit('chat message', socket.username + ": " + msg); //Servern tar emot det vi skrivit in och skriver ut vad användaren har skrivit.
+
+    });
+    socket.on('disconnect', (user) => {
+        socket.broadcast.emit('newUser', socket.username + ' Disconnected')
+    });
+});
+
+http.listen(5000, function () {
+    console.log('listening on *:5000');
+});
