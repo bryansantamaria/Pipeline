fb3a84312da5ca4e6c796917d71a629402662934
Fabian Johansson <fabulo1998@gmail.com>
2019-12-05 15:10:19 +0100

    fixed merge

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,28 @@
 import {
   ChatModule
 } from "./chat-module.js";
-let chatMessages = [];
-let chatModule = new ChatModule(
-  'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.',
-  'Fabian Johansson',
-  'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/74617601_2692609500763768_5275050151155597312_o.jpg?_nc_cat=103&_nc_ohc=XBoYWDubAUUAQmTc4DVWQe_05mCnZ0CL3rXU91R8LK5tH670PwiZwEaog&_nc_ht=scontent-arn2-1.xx&oh=d53991ce63884e4faa86b246edbff50b&oe=5E7797AA',
-  '11:26'
-);
+let chatMessages = [
+  new ChatModule(
+    'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.',
+    'Fabian Johansson',
+    'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/74617601_2692609500763768_5275050151155597312_o.jpg?_nc_cat=103&_nc_ohc=XBoYWDubAUUAQmTc4DVWQe_05mCnZ0CL3rXU91R8LK5tH670PwiZwEaog&_nc_ht=scontent-arn2-1.xx&oh=d53991ce63884e4faa86b246edbff50b&oe=5E7797AA',
+    '11:26'
+  ),
+  new ChatModule(
+    'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.',
+    'Bryan Santamaria',
+    'https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/13580553_10153540477521470_8547081405158753156_o.jpg?_nc_cat=111&_nc_ohc=w1OeQC-JqpUAQnyq8OfuNAvLuI8YrDBMTmZkQo4vtQl-uAZIjfdGDN2lw&_nc_ht=scontent-arn2-1.xx&oh=20045fa1123f617259ecb2a4d768ad27&oe=5E83EB6D',
+    '11:26'
+  ),
+  new ChatModule(
+    'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.',
+    'Alexander Wilson',
+    'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/51721821_10156909845079030_3534192012213354496_n.jpg?_nc_cat=109&_nc_ohc=CabSkySieYMAQlIvjyAJrkabzdmPpbzrfdZN9QL78W5gjN_cxQuE_-7Qg&_nc_ht=scontent-arn2-1.xx&oh=d57a7ebc3db7466ef4e5ec2822247038&oe=5E830A5E',
+    '11:26'
+  )
+  
+];
 
-let chatModule2 = new ChatModule(
-  'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.',
-  'Bryan Santamaria',
-  'https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/13580553_10153540477521470_8547081405158753156_o.jpg?_nc_cat=111&_nc_ohc=w1OeQC-JqpUAQnyq8OfuNAvLuI8YrDBMTmZkQo4vtQl-uAZIjfdGDN2lw&_nc_ht=scontent-arn2-1.xx&oh=20045fa1123f617259ecb2a4d768ad27&oe=5E83EB6D',
-  '11:26'
-);
-
-let chatModule3 = new ChatModule(
-  'Aliquam elit eros, suscipit quis semper eget, consectetur eget nisi. Donec consectetur quis nibh eget viverra. Aenean pulvinar mollis arcu, porta faucibus nibh pellentesque sit amet. Ut non tristique lorem, ut maximus mi. Quisque iaculis elit sed risus ultrices, blandit iaculis neque scelerisque.',
-  'Alexander Wilson',
-  'https://scontent-arn2-1.xx.fbcdn.net/v/t1.0-9/51721821_10156909845079030_3534192012213354496_n.jpg?_nc_cat=109&_nc_ohc=CabSkySieYMAQlIvjyAJrkabzdmPpbzrfdZN9QL78W5gjN_cxQuE_-7Qg&_nc_ht=scontent-arn2-1.xx&oh=d57a7ebc3db7466ef4e5ec2822247038&oe=5E830A5E',
-  '11:26'
-);
 
 
 //Globals
@@ -104,15 +105,19 @@ function getTodaysDate(date) {
   return rightNow;
 }
 
-document.querySelector('#create-private-message').setAttribute("data-toggle","modal")
-document.querySelector('#create-private-message').setAttribute("data-target", "#create-pm-modal")
+//Cursed bootstrap attributes
+document.querySelector('#private-message-title').setAttribute("data-toggle","modal")
+document.querySelector('#private-message-title').setAttribute("data-target", "#create-pm-modal")
 
 document.querySelector('#user-settings').setAttribute("data-toggle","modal")
 document.querySelector('#user-settings').setAttribute("data-target", "#edit-profile-modal")
 
-chatModule.render(document.querySelector('message-root'));
-chatModule2.render(document.querySelector('message-root'));
-chatModule3.render(document.querySelector('message-root'));
+document.querySelector('user-name').setAttribute("data-toggle","modal")
+document.querySelector('user-name').setAttribute("data-target", "#edit-profile-modal")
+
+chatMessages.forEach(msg => {
+  msg.render(document.querySelector('message-root'));
+})
 
 
 $(document).ready(() => {
