327d413872c2db1d1198970268f08d99d4cfaf7f
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-12-06 15:27:09 +0100

    Changed id's and added pointers to a tags at sign in & create acc page.

--- public/js/index.js
+++ public/js/index.js
@@ -4,21 +4,15 @@ $('#signUpBtn').click((e) => {
     window.location.assign("http://127.0.0.1:5000/chat");
 });
 
-let loginHtml = `
-
-`;
-
 $(document).ready(() => {
     $('create-account').hide();
 
-    $('#registrer').on("click", e => {
-        e.preventDefault();
+    $('#register').on("click", e => {
         $('create-account').fadeIn();
         $('login-container').hide();
     })
 
-    $('#to-login-btn').on("click", e => {
-        e.preventDefault();
+    $('#login-link').on("click", e => {
         $('create-account').hide();
         $('login-container').fadeIn();
     })
