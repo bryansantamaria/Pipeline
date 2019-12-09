a51558f2888ab9a045cb67baaf16321cc1be9abf
Fabian Johansson <fabulo1998@gmail.com>
2019-12-09 19:34:04 +0100

    cleaned up app.js

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const io = require('socket.io')(http);
 const path = require('path');
 const bodyParser = require("body-parser");
 const request = require('request-promise');
-// TODO: Move to server.js
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-// TODO: Move to server.js
-app.use(bodyParser.json());
 
+///////////////////////////////////////////////////
+/// MIDDLEWARES
+///////////////////////////////////////////////////
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,106 +22,109 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+///////////////////////////////////////////////////
+/// ROUTES
+///////////////////////////////////////////////////
+
+
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+    res.render('index.ejs');
 });
 
-// TODO: Move to server.js
 app.post('/register', (req, res) => {
-  /*let user = await */
-  request('http://127.0.0.1:3000/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(req.body),
-  }).then(() => {
-    res.redirect('chat');
-  });
+    request('http://127.0.0.1:3000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body),
+    }).then(() => {
+        res.redirect('chat');
+    });
 });
 
-/*app.post('/chatroom', (req, res) => {
-
-});*/
-
-// TODO: Move to server.js
 app.get('/chat', (req, res) => {
-  request('http://127.0.0.1:3000/chat', {
-    method: 'get',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(users => {
-    res.render('chat', { "users": JSON.parse(users) });
-  });
+    request('http://127.0.0.1:3000/chat', {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(users => {
+        res.render('chat', { "users": JSON.parse(users) });
+    });
 });
 
 app.post('/login', (req, res) => {
-  console.log(req.body);
-  request('http://127.0.0.1:3000/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(req.body),
-  }).then((authorized) => {
-    if (JSON.parse(authorized)) {
-      res.redirect('chat');
-    } else {
-      res.redirect('/');
-    }
-  });
+    console.log(req.body);
+    request('http://127.0.0.1:3000/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(req.body),
+    }).then((authorized) => {
+        if (JSON.parse(authorized)) {
+            res.redirect('chat');
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
+///////////////////////////////////////////////////
+/// SOCKET CONFIG
+///////////////////////////////////////////////////
+
 io.on('connection', (socket) => {
-  socket.on('newUser', (user) => {
-    socket.username = user;
-    //New user is online
-    socket.broadcast.emit('newUser', `${user}: Is now online!`);
-    //You are online
-    socket.on('userOnline', (user) => {
-      socket.emit('userOnline', `You ${user} are online`);
-    });
-    // is Typing
-    socket.on('typing', (isTyping) => {
-      socket.broadcast.emit('updateTyping', user, isTyping);
-    });
-  });
-
-  socket.on('chat message', function (chatObject) { //Lyssnar på eventet 'chat message'
-    request('http://127.0.0.1:3000/chatroom', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: chatObject,
-    }).then(message => {
-      console.log(JSON.parse(message));
-      //The server recieves a JSON string object and sends it further to all clients connected to the socket.
-      io.emit('chat message', JSON.parse(message));
+    socket.on('newUser', (user) => {
+        socket.username = user;
+        //New user is online
+        socket.broadcast.emit('newUser', `${user}: Is now online!`);
+        //You are online
+        socket.on('userOnline', (user) => {
+            socket.emit('userOnline', `You ${user} are online`);
+        });
+        // is Typing
+        socket.on('typing', (isTyping) => {
+            socket.broadcast.emit('updateTyping', user, isTyping);
+        });
     });
 
-    //HTTP request till servern, Post request fetch
-    /*async function getMessage(msg) {
-      let customers = await fetch('http://127.0.0.1:3001/chat/' + msg, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'text/json'
-        }
-      }).then(data => {
-        return data.json();
-      });
+    socket.on('chat message', function (chatObject) { //Lyssnar på eventet 'chat message'
+        request('http://127.0.0.1:3000/chatroom', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: chatObject,
+        }).then(message => {
+            console.log(JSON.parse(message));
+            //The server recieves a JSON string object and sends it further to all clients connected to the socket.
+            io.emit('chat message', JSON.parse(message));
+        });
 
-      render(customers);
-    }*/
+        //HTTP request till servern, Post request fetch
+        /*async function getMessage(msg) {
+          let customers = await fetch('http://127.0.0.1:3001/chat/' + msg, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+              'Content-Type': 'text/json'
+            }
+          }).then(data => {
+            return data.json();
+          });
+    
+          render(customers);
+        }*/
 
-  });
-  socket.on('disconnect', (user) => {
-    socket.broadcast.emit('newUser', socket.username + ' Disconnected')
-  });
+    });
+    socket.on('disconnect', (user) => {
+        socket.broadcast.emit('newUser', socket.username + ' Disconnected')
+    });
 });
 
 http.listen(5000, function () {
-  console.log('listening on *:5000');
+    console.log('listening on *:5000');
 });
+
