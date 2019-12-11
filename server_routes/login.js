1b7e834ac5f773eb02fba8093f9d7e1cd16469d8
Bryan Santamaria <bryaan.santamaria@gmail.com>
2019-12-11 18:35:03 +0100

    hashed users password into user DB, I now need to validate it

--- server_routes/login.js
+++ server_routes/login.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
     var pipelineDB = req.db;
     var collection = pipelineDB.get("users");
     collection.find({ $or: [{ "alias": req.body.username}, { "email": req.body.username}] }, {}).then(user => {
-        console.log(user);
+        
+        console.log('USER: '+user);
+        bcrypt.compare(req.body.password, hashedPassword, (err, res) => {
+            if (err) throw err;
+            console.log('Password correct: ', res);
+        });
         if (user[0]) {
             if (user[0].password == req.body.password) {
                 res.send(user[0]);
--- server_routes/register.js
+++ server_routes/register.js
@@ -2,28 +2,45 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
+// router.post('/', (req, res) => {
+//     var pipelineDB = req.db;
+//     var collection = pipelineDB.get("users");
+//     collection.insert({
+//         "email": req.body.email,
+//         "password": req.body.password,
+//         "alias": req.body.username
+//     }, (err, user_in_db) => {
+//         if (err) throw err;
+//         res.send(JSON.stringify(user_in_db));
+//     });
+// });
 router.post('/', (req, res) => {
     var pipelineDB = req.db;
-    var collection = pipelineDB.get("users");
-    collection.insert({
-        "email": req.body.email,
-        "password": req.body.password,
-        "alias": req.body.username
-    }, (err, user_in_db) => {
+
+    var saltRounds = 10;
+    let password = req.body.password;
+    console.log('password: '+password);
+
+    bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
         if (err) throw err;
-        res.send(JSON.stringify(user_in_db));
+        console.log('hashedPassword: '+hashedPassword);
+        var collection = pipelineDB.get("users");
+        collection.insert({
+            "email": req.body.email,
+            "password": hashedPassword,
+            "alias": req.body.username
+        }, (err, user_in_db) => {
+            if (err) throw err;
+            res.send(JSON.stringify(user_in_db));
+        });
     });
+    bcrypt.compare(req.body.password, password, (err, res) => {
+        if (err) throw err;
+        console.log('Password correct: ', res);
+    });
+
 });
 
-var saltRounds = 10;
-let hashedPassword = req.body.password;
-bcrypt.hash(hashedPassword, saltRounds, (err, hash) => {
-    if(err) throw err;
-        console.log(hash);
-});
-bcrypt.compare(req.body.password, hashedPassword, (err, res) => {
-    if(err) throw err;
-    console.log('Password correct: ', res);
-});
+
 
 module.exports = router;
